4e97f5a0ea30605f19bdb3cb4dcdcfce444afdc6
wwartick <willwartick@gmail.com>
2024-02-04 08:39:21 -0800

    create book

--- script.js
+++ script.js
@@ -2,12 +2,18 @@ const addBookButton = document.querySelector('.add-book-button');
 const removeButton = document.querySelectorAll('.remove');
 const readButton = document.querySelectorAll('.change-read-status');
 const dialog = document.querySelector('dialog');
+const cancelBtn= dialog.querySelector('.cancel-button');
+const submitBtn = dialog.querySelector('.submit-button');
+let title = dialog.querySelector("#title");
+let author = dialog.querySelector("#author");
+let pages = dialog.querySelector("#pages");
+let readStatus = dialog.querySelector('#readStatus');
 const myLibrary =[];
 
 function Book(title, author, pages, status) {
-    this.title=title;
+    this.title = title;
     this.author = author;
-    this.pages = pages;
+    this.pages = pages; 
     this.status = status;
 }
 
@@ -17,11 +23,31 @@ function changeReadStatus() {
 function removeCard() {
     console.log(event.target);
 }
-function addBookToLibrary() {
-    dialog.showModal();
-}
 
+function createBook(e){
+    let bookTitle= title.value;
+    let bookAuthor = author.value;
+    let bookPages= pages.value;
+    let bookStatus; 
+
+    if(readStatus.checked === true ? bookStatus = true : bookStatus=false);
+    if(title.value === '' || author.value === '' || pages.value === ''){
+       return;
+    }   else{
+        myLibrary.push(new Book(bookTitle, bookAuthor, bookPages, bookStatus));
+    }
+    e.preventDefault();
+}
 
 readButton.forEach(button => button.addEventListener('click', changeReadStatus))
 removeButton.forEach(button => button.addEventListener('click', removeCard))
-addBookButton.addEventListener('click', addBookToLibrary);
+addBookButton.addEventListener('click', () => {dialog.showModal()});
+submitBtn.addEventListener('click', createBook)
+cancelBtn.addEventListener('click', (e) => { 
+    e.preventDefault();
+    title.value ='';
+    author.value ='';
+    pages.value ='';
+    readStatus.checked=false;
+    dialog.close();
+})
